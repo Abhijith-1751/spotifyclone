51ec8215e1b98e6701911a47d67984bb0cad8171
Abhijith-1751 <72120509+Abhijith-1751@users.noreply.github.com>
2023-05-13 11:59:56 +0530

    2 files modified

--- compontents/songcompond.tsx
+++ compontents/songcompond.tsx
@@ -6,7 +6,9 @@ function Songcompond(){
   return(
    <div className="container">
      <img src="https://i.ytimg.com/vi/imysuB0twTg/sddefault.jpg" alt="img"/>
-
+     <audio controls>
+     <source src="https://naasongs.vip/myuploads/uploads/Varisu/Jimikki%20Ponnu.mp3"/>
+    </audio>
    </div>
   );
 }
