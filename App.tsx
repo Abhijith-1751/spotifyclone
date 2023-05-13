4d3452050c89c6dd76f4d363ee2e9e86e7376f09
Abhijith-1751 <72120509+Abhijith-1751@users.noreply.github.com>
2023-05-13 11:01:23 +0530

    1 file modified, 2 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import './style.css';
 import Navbar from './compontents/navbar';
+import Contentbox from './compontents/contentbox';
 
 export default function App() {
   return (
     <div className="app">
       <Navbar />
+      <Contentbox/>
     </div>
   );
 }
---
+++ compontents/contentbox.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import '../css/contentbox.css'
+
+
+function Contentbox(){
+  return( 
+    <div className="content-box">
+
+    </div>
+  );
+}
+
+export default Contentbox;
