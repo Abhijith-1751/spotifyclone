11bb35f32d688ca9ef3d67605ab532e5d8add466
Abhijith-1751 <72120509+Abhijith-1751@users.noreply.github.com>
2023-05-13 12:19:06 +0530

    2 files modified

--- compontents/contentbox.tsx
+++ compontents/contentbox.tsx
@@ -6,7 +6,7 @@ import Songcompond from './songcompond';
 function Contentbox(){
   return( 
     <div className="content-box">
-      <Songcompond/>
+      <Songcompond imglink="https://i.ytimg.com/vi/imysuB0twTg/sddefault.jpg" audiosrc="https://naasongs.vip/myuploads/uploads/Varisu/Jimikki%20Ponnu.mp3"/>
       <Songcompond/>
       <Songcompond/>
       <Songcompond/>
--- compontents/songcompond.tsx
+++ compontents/songcompond.tsx
@@ -2,9 +2,14 @@ import * as React from 'react'
 import '../css/Songcompond.css'
 
 
-function Songcompond(){
+function Songcompond(imglink: string,audiosrc: string){
   return(
    <div className="container">
+     {/* <img src={imglink} alt={imglink}/>
+     <audio controls>
+       <source src={audiosrc}/>
+    </audio> */}
+
      <img src="https://i.ytimg.com/vi/imysuB0twTg/sddefault.jpg" alt="img"/>
      <audio controls>
      <source src="https://naasongs.vip/myuploads/uploads/Varisu/Jimikki%20Ponnu.mp3"/>
