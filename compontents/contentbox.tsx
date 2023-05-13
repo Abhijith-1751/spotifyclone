e1897b59bffe0951f53e96e2e9fd608c9a79628f
Abhijith-1751 <72120509+Abhijith-1751@users.noreply.github.com>
2023-05-13 11:12:31 +0530

    2 files modified, 2 added

--- compontents/contentbox.tsx
+++ compontents/contentbox.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react'
 import '../css/contentbox.css'
+import Songcompond from './songcompond';
 
 
 function Contentbox(){
   return( 
     <div className="content-box">
-
+      <Songcompond/>
+      <Songcompond/>
+      <Songcompond/>
+      <Songcompond/>
+      <Songcompond/>
+      <Songcompond/>
     </div>
   );
 }
+
 
 export default Contentbox;
---
+++ compontents/songcompond.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import '../css/Songcompond.css'
+
+
+function Songcompond(){
+  return(
+   <div className="container">
+
+   </div>
+  );
+}
+
+export default Songcompond
